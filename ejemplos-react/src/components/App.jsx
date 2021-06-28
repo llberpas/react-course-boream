919fc7b7223d7862f4665a4f0ee1c15f64d006df
Lluis Bernabeu <Lluis.BernabeuPastor@zooplus.com>
2021-06-28 13:14:30 +0200

    Día 1: intro, configuracion con webpack y babel, tipos de componentes y estilos

---
+++ ejemplos-react/src/components/App.jsx
@@ -0,0 +1,15 @@
+import Componentes from './Components'
+import CmpDeClase from './CmpDeClase'
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        <Componentes />
+        <CmpDeClase />
+        </h1>
+    </div>
+  )
+}
+
+export default App
